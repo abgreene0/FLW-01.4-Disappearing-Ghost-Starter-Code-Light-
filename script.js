e647d2a3b56cf2c90d056f96ba6b8e64fe19040e
abgreene0 <105506623+abgreene0@users.noreply.github.com>
2022-11-08 00:23:44 +0000

    .

--- script.js
+++ script.js
@@ -1,1 +1,35 @@
+//html element and selector variables
+let p = document.createElement("p");
+let h1 = document.querySelector(".header");
+let input = document.querySelector(".input");
+let hide = document.querySelector(".hide-button");
+let show = document.querySelector(".show-button");
+let update = document.querySelector(".update-img-button");
+let message = document.querySelector(".message-button");
+let name = document.querySelector(".name-button");
+let userInput;
 
+
+//create an onclick event for the Hide Me button
+/*hide button variable*/
+hide.onclick = (function (){  
+    ghostImage
+});
+
+//create an onclick event for the Show Me button
+show.onclick = (function (){  
+
+
+//create an onclick event for the Update Img button
+update.onclick = (function (){  
+
+
+//create an onclick event for the Send Message button
+//update the userInput variable by saving the value of the input
+//use the insertAdjacentHTML method to append the userInput in a <p> tag
+
+
+
+//create an onclick event for the Name Me button
+//update the userInput variable by saving the value of the input
+//use the innerHTML method to update the h1 tag
